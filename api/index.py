2d74782484581ddb905a32fa800efd35665de34b
Aman Rawat <kirmolaa@gmail.com>
2024-05-19 20:23:18 +0530

    added python slugify

--- api/index.py
+++ api/index.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from slugify import slugify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/', methods=['POST'])
+def slugify_text():
+    """
+    Endpoint to accept POST requests and slugify text.
+    """
+    if request.method == 'POST':
+        text = request.form.get('text', '')  # Get text from form data
+        slugified_text = slugify(text)
+        return jsonify({'slugified_text': slugified_text}), 200
